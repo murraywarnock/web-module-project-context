a2ad5a1925403bbf8b89ecca963ab6fe3630e75a
MurrayWarnock <murray.warnock@gmail.com>
2021-06-10 16:01:17 -0600

    MVP requirements met

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { NavLink } from 'react-router-dom';
+import CartContext from "../contexts/CartContext"
 
-const Navigation = props => {
+const Navigation = () => {
+	const cart = useContext(CartContext);
+	console.log("cart in navigation: ", cart )
+	console.log("But cart.length is undefined!: ", cart.length)
 	return (
 		<div className="navigation">
 			<NavLink to="/">Products</NavLink>
-			<NavLink to="/cart">
-				Cart <span>{props.cart.length}</span>
+			<NavLink to="/cart">Cart <span>{cart.length}</span>
 			</NavLink>
 		</div>
 	);
